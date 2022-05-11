ce5eaa9c4e4596b8b5c0fccdc7e1e454f0dfff0f
Rania21-jebali <raniajebali21@gmail.com>
2022-05-11 21:57:29 +0100

    update Ajouter formation

--- client/src/backOffice/pages/AddFormation/Programme.js
+++ client/src/backOffice/pages/AddFormation/Programme.js
@@ -14,6 +14,8 @@ import AddIcon from '@material-ui/icons/Add';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import {  Modal} from 'antd';
+import CloseIcon from '@material-ui/icons/Close';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 
 const { confirm } = Modal;
 const { Panel } = Collapse;
@@ -76,7 +78,7 @@ const { Panel } = Collapse;
                 }
 
         return(
-            <div className='content-chapitre'>
+            <>
                 <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" >
                     <Form.Label className="label">Titre du section</Form.Label>
@@ -102,10 +104,163 @@ const { Panel } = Collapse;
                   <Button  className='btn-confirme'  type="submit">Enregistrer la section</Button>
                 </div>
               </Form>
-          </div>
+          </>
         )
     }
- 
+
+    function AddSession(){
+      return(
+        <div>
+                <Form >
+                    <Form.Group className="mb-3" >
+                    <Form.Label className="label">Nouvelle session</Form.Label>
+                        <Form.Control type="text" 
+                        placeholder="Entrer un titre" 
+                        />
+                    </Form.Group>
+                <div className="content-btn">
+                  <Button className='btn-annnuler'>Annuler</Button>
+                  <Button  className='btn-confirme'  type="submit">Ajouter une session</Button>
+                </div>
+              </Form>
+                </div>
+      )
+    }
+
+    function AddDescription(){
+      return(
+        <div>
+                <Form >
+                    <Form.Group className="mb-3" >
+                    <Form.Label className="label">Description de la session</Form.Label>
+                        <Form.Control type="text" 
+                        placeholder="Ajouter une description.Mentionnez ce que les participants seront capables de faire une fois la session" 
+                        />
+                    </Form.Group>
+                <div className="content-btn">
+                  <Button className='btn-annnuler'>Annuler</Button>
+                  <Button  className='btn-confirme'  type="submit">Sauvegarder</Button>
+                </div>
+              </Form>
+                </div>
+      )
+    }
+
+    function AddVideo(){
+      return(
+        <div>
+                <Form >
+                    <Form.Group className="mb-3" >
+                    <Form.Label className="label">Télécharger vidéo :</Form.Label>
+                        <Form.Control type="file" 
+                        />
+                    </Form.Group>
+                <div className="content-btn">
+                  <Button className='btn-annnuler'>Annuler</Button>
+                  <Button  className='btn-confirme'  type="submit">Sauvegarder</Button>
+                </div>
+              </Form>
+                </div>
+      )
+    }
+
+    function AddMulti(){
+      return(
+        <div>
+                <Form >
+                    <Form.Group className="mb-3" >
+                    <Form.Label className="label">Choisissez une vidéo :</Form.Label>
+                        <Form.Control type="file" 
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" >
+                    <Form.Label className="label">Choisissez une présentation :</Form.Label>
+                        <Form.Control type="file" 
+                        />
+                    </Form.Group>
+                <div className="content-btn">
+                  <Button className='btn-annnuler'>Annuler</Button>
+                  <Button  className='btn-confirme'  type="submit">Sauvegarder</Button>
+                </div>
+              </Form>
+                </div>
+      )
+    }
+
+    function AddArticle(){
+      return(
+        <div>
+                <Form >
+                    <Form.Group className="mb-3" >
+                    <Form.Label className="label">Texte</Form.Label>
+                        <Form.Control type="text" 
+                          placeholder="Ecrire text..." 
+                        />
+                    </Form.Group>
+                <div className="content-btn">
+                  <Button className='btn-annnuler'>Annuler</Button>
+                  <Button  className='btn-confirme'  type="submit">Sauvegarder</Button>
+                </div>
+              </Form>
+                </div>
+      )
+    }
+
+    function AddContenu(){
+      const [video, setVideo] = useState(false)
+      const [article, setArticle] = useState(false)
+      const [multi, setMulti] = useState(false)
+
+      return(
+        <div>
+      <h5>Sélectionner le type de contenu</h5>
+      <p>Sélectionner le type principal de contenu. Des fichiers et des liens peuvent
+      <br /> être ajoutés en tant que ressources.</p>
+      {
+        (!video && !multi && !article) &&
+        (
+          <div className='content-type'>
+            <div className='type-content' onClick={ () => setVideo(!video)}>
+              <PlayCircleOutlineIcon /><p>Vidéo</p>
+            </div>
+            <div className='type-content' onClick={ () => setMulti(!multi)}>
+              <p>Support:<br /> Vidéo et <br />diapositives</p>
+            </div>
+            <div className='type-content' onClick={ () => setArticle(!article)}>
+              <DescriptionIcon /><p>Article</p>
+            </div>
+          </div>
+        )
+      }
+      
+      {
+        video && 
+        (<div className='content-chapitre'>
+                  <CloseIcon onClick={ () => setVideo(!video)}/>
+                    <AddVideo />
+                </div>
+                  )
+      }
+      {
+        multi && 
+        (<div className='content-chapitre'>
+                  <CloseIcon onClick={ () => setMulti(!multi)}/>
+                    <AddMulti />
+                </div>
+                  )
+      }
+      {
+        article && 
+        (<div className='content-chapitre'>
+                  <CloseIcon onClick={ () => setArticle(!article)}/>
+                    <AddArticle />
+                </div>
+                  )
+      }
+        </div>
+      )
+    }
+
     function AfficheSection(){
         const token = useSelector(state => state.token)
         const formations = useSelector(state => state.formations)
@@ -116,6 +271,9 @@ const { Panel } = Collapse;
         const [callback2, setCallback2] = useState(false)
         const [chapitre, setChapitre] = useState(initialState)
         const [callback3, setCallback3] = useState(false)
+        const [description, setDescription] = useState(false)
+        const [contenu, setContenu] = useState(false)
+        const [session, setSession] = useState(false)
         const dispatch = useDispatch()
         const dispatch2 = useDispatch()
         const {titre1} = useParams();
@@ -162,11 +320,7 @@ const { Panel } = Collapse;
                       },
                     });
                 }
-
-            const genExtra = () => (
-                    <Button className='btn-annnuler'><AddIcon />Contenu</Button>
-                );
-
+           
             const getHeader = () => (
                       <div  onMouseEnter={() => setEdit1(true)}  onMouseLeave={() => setEdit1(false)}>
                           <h5>
@@ -193,13 +347,44 @@ const { Panel } = Collapse;
                 <DeleteOutlineIcon className="icon-prog" onClick={showDeleteConfirm}/></>)}</h5>
           <div>
             <Collapse  onChange={callback1}>
-                <Panel header={getHeader()} extra={genExtra()} >
+                <Panel header={getHeader()} >
+                
+                { (!description && !contenu) && (
+                  <>
+                  <Button className='btn-annnuler' onClick={ () => setDescription(!description)}>
+                <AddIcon />Description</Button>
+                <Button className='btn-annnuler' onClick={() => setContenu(!contenu)}>
+                <AddIcon />Contenu</Button>
+                  </>
+                 )
+                }
+                { description && 
+                (<div className='content-chapitre'>
+                  <CloseIcon onClick={ () => setDescription(!description)}/>
+                    <AddDescription />
+                </div>
+                  )}
+                {
+                  contenu && (<div className='content-chapitre'>
+                  <CloseIcon onClick={ () => setContenu(!contenu)}/>
+                    <AddContenu />
+                </div>
+                  )
+                }
                   
                 </Panel>
             </Collapse>
             <div className='btn-add-session'>
-            <p><AddCircleOutlineIcon className='icon-add'/>Ajouter session</p>
+            <p><AddCircleOutlineIcon className='icon-add' onClick={() => setSession(!session)}/>Ajouter session</p>
             </div>
+            {
+              session && (
+                <div className='content-chapitre'>
+                  <CloseIcon onClick={ () => setSession(!session)}/>
+                    <AddSession />
+                </div>
+              )
+            }
             </div>
           </div> 
           
@@ -239,11 +424,16 @@ function Programme() {
          des sessions  et des exercices pratiques <br />(quiz, exercices de codage et exercices).</p>
         <p className='paragraphe'>Si vous prévoyez de rendre votre cours gratuit,
         la longueur totale de son contenu vidéo doit être inférieure à 2 heures.</p>
-        <Button className='btn-confirme' onClick={() => setShow(!show)} ><AddIcon />Ajouter section</Button>
          <AfficheSection />
         {
-          !show && <AddSection className="icon-prog"/>
-        } 
+          !show && (
+            <div className='content-chapitre'>
+            <CloseIcon onClick={ () => setShow(!show)}/>
+            <AddSection className="icon-prog"/>
+            </div>
+        )} 
+        <Button className='btn-confirme' onClick={() => setShow(!show)} ><AddIcon />Ajouter section</Button>
+
     </div>
   )
 }
